1695e06cf4b8eb13e78694d63ae3f7c35b4e53af
jonbir3 <jonbir3@gmail.com>
2019-03-29 19:56:11 +0300

    initial project

---
+++ app/src/main/java/com/funtestic/activities/MainActivity.java
@@ -0,0 +1,14 @@
+package com.funtestic.activities;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import com.funtestic.R;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main_activity_layout);
+    }
+}
