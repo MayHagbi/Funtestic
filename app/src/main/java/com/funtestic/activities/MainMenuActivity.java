262df4b476ea08911bb9dc1f5f1e0bc7a8bc9fd5
Yoel Vaizman <yoelv92@gmail.com>
2019-03-30 20:17:12 +0300

    put Main Manu and Report activities

---
+++ app/src/main/java/com/funtestic/activities/MainMenuActivity.java
@@ -0,0 +1,56 @@
+package com.example.funtestic;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.View;
+import android.widget.Button;
+
+public class MainMenuActivity extends AppCompatActivity implements View.OnClickListener {
+
+    private Button startButton;
+    private Button reportButton;
+    private Button exitButton;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main_menu);
+
+
+        startButton = (Button)findViewById(R.id.mainMenuStartButton);
+        reportButton=(Button)findViewById(R.id.mainMenuReportButton);
+        exitButton=(Button)findViewById(R.id.mainMenuExitButton);
+
+        startButton.setOnClickListener(this);
+        reportButton.setOnClickListener(this);
+        exitButton.setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View view){
+
+        switch(view.getId()) {
+            case R.id.mainMenuStartButton:
+                finish();
+                startActivity(new Intent(getApplicationContext() , MainActivity.class));
+                break;
+
+            case R.id.mainMenuReportButton:
+                //finish();
+                startActivity(new Intent(getApplicationContext() , ReportActivity.class));
+                break;
+            case R.id.mainMenuExitButton:
+                this.finish();
+                System.exit(0);
+                break;
+        }
+    }
+
+
+}
+
+
