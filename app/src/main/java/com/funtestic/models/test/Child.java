af95bfd172cae1ac33a6833d6869773585c774c7
Yoel Vaizman <yoelv92@gmail.com>
2019-03-30 21:31:03 +0300

    create parent class in test pakegpakeg

---
+++ app/src/main/java/com/funtestic/models/test/Child.java
@@ -0,0 +1,10 @@
+package com.funtestic.models.test;
+
+import android.support.annotation.NonNull;
+
+import java.io.Serializable;
+import java.sql.Date;
+
+public class Child {
+
+}
---
+++ app/src/main/java/com/funtestic/models/test/Parent.java
@@ -0,0 +1,8 @@
+package com.funtestic.models.test;
+
+import java.util.*; 
+
+
+public class Parent {
+	
+}
