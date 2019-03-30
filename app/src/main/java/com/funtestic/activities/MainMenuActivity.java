b67558f9b1412dd7e8fd139467c33070aa59e1f5
Yoel Vaizman <yoelv92@gmail.com>
2019-03-30 20:17:22 +0300

    put Main Manu and Report activities

--- app/src/main/java/com/funtestic/activities/MainMenuActivity.java
+++ app/src/main/java/com/funtestic/activities/MainMenuActivity.java
@@ -1,13 +1,12 @@
-package com.example.funtestic;
+package com.funtestic.activities;
 
 import android.content.Intent;
 import android.os.Bundle;
-import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
-import android.support.v7.widget.Toolbar;
 import android.view.View;
 import android.widget.Button;
+
+import com.funtestic.R;
 
 public class MainMenuActivity extends AppCompatActivity implements View.OnClickListener {
 
--- app/src/main/java/com/funtestic/activities/ReportActivity.java
+++ app/src/main/java/com/funtestic/activities/ReportActivity.java
@@ -1,17 +1,14 @@
-package com.example.funtestic;
+package com.funtestic.activities;
 
 import android.os.Bundle;
-import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
-import android.support.v7.widget.Toolbar;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
-import android.widget.EditText;
-import android.widget.ScrollView;
 import android.widget.Spinner;
 import android.widget.TextView;
+
+import com.funtestic.R;
 
 import java.util.ArrayList;
 import java.util.List;
