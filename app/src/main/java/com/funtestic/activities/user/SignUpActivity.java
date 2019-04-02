ef7280745b12c0dc87f157b7a059922748dfff88
jonbir3 <jonbir3@gmail.com>
2019-04-02 23:58:48 +0300

    sign up activity and layout

--- app/src/main/java/com/funtestic/activities/user/SignUpActivity.java
+++ app/src/main/java/com/funtestic/activities/user/SignUpActivity.java
@@ -4,7 +4,6 @@ import android.app.ProgressDialog;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
-import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
@@ -14,23 +13,20 @@ import com.funtestic.R;
 //import butterknife.BindView;
 //import butterknife.ButterKnife; //TODO: how it work?
 
-//TODO: move hardcoded strings to strings resources
-
-public class SignUpActivity extends AppCompatActivity {
-    private static final String TAG = "SignUpActivity";
-    private static final int MIN_CHARACTERS_TO_NAME = 2;
-    private static final int MIN_PASSWORD_LENGTH = 8;
+public class SignUpActivity extends AppCompatActivity implements View.OnClickListener {
 
 //    @BindView(R.id.input_name)
-    private EditText nameText;
+    private EditText etName;
 //    @BindView(R.id.input_email)
-    private EditText emailText;
+    private EditText etEmail;
 //    @BindView(R.id.input_password)
-    private EditText passwordText;
+    private EditText etPassword;
 //    @BindView(R.id.btn_signUp)
-    private Button signUpButton;
+    private Button btnSignUp;
 //    @BindView(R.id.link_login)
     private TextView loginLink;
+    private EditText etId;
+    private ProgressDialog signUpProgressDialog;
 
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
@@ -38,104 +34,79 @@ public class SignUpActivity extends AppCompatActivity {
         setContentView(R.layout.signup_activity_layout);
 //        ButterKnife.bind(this);
 
-        nameText = (EditText) findViewById(R.id.input_name);
-        emailText = (EditText) findViewById(R.id.input_email);
-        passwordText = (EditText) findViewById(R.id.input_password);
-        signUpButton = (Button) findViewById(R.id.btn_signUp);
-        loginLink = (TextView) findViewById(R.id.link_login);
-
-        signUpButton.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                signUp();
-            }
-        });
-
-        loginLink.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                //TODO: Finish the registration screen and return to the Login activity
-                finish();
-            }
-        });
+        etName = (EditText) findViewById(R.id.input_name_sign_up);
+        etEmail = (EditText) findViewById(R.id.input_email_sign_up);
+        etPassword = (EditText) findViewById(R.id.input_password_sign_up);
+        btnSignUp = (Button) findViewById(R.id.btn_sign_up);
+        loginLink = (TextView) findViewById(R.id.link_login_sign_up_activity);
+        etId = (EditText) findViewById(R.id.input_id_sign_up);
+        signUpProgressDialog = new ProgressDialog(this);
+        btnSignUp.setOnClickListener(this);
+        loginLink.setOnClickListener(this);
     }
 
     public void signUp() {
-        Log.d(TAG, "signUp");
+        btnSignUp.setEnabled(false);
 
         if (!validate()) {
             onSignUpFailed();
             return;
         }
 
-        signUpButton.setEnabled(false);
-
         //TODO: ProgressDialog deprecated, try to use progressbar
-        final ProgressDialog progressDialog = new ProgressDialog(SignUpActivity.this,
-                R.style.Theme_AppCompat_Light_Dialog); //TODO: the original theme was AppTheme_Dark_Dialog
-        progressDialog.setIndeterminate(true);
-        progressDialog.setMessage("Creating Account...");
-        progressDialog.show();
+        signUpProgressDialog.setIndeterminate(true);
+        signUpProgressDialog.setMessage(getString(R.string.creating_account));
+        signUpProgressDialog.show();
 
-        String name = nameText.getText().toString();
-        String email = emailText.getText().toString();
-        String password = passwordText.getText().toString();
+        if(!addUserToDB()) {
+            onSignUpFailed();
+        }
 
-        // TODO: Implement your own sign up logic here.
-
-        new android.os.Handler().postDelayed( //TODO: necessary?
-                new Runnable() {
-                    public void run() {
-                        // On complete call either onSignUpSuccess or onSignUpFailed
-                        // depending on success
-                        onSignUpSuccess();
-                        // onSignUpFailed();
-                        progressDialog.dismiss();
-                    }
-                }, 3000);
-    }
-
-
-    public void onSignUpSuccess() {
-        signUpButton.setEnabled(true);
-        setResult(RESULT_OK, null);
+        btnSignUp.setEnabled(true);
         finish();
-    }
-
-    public void onSignUpFailed() {
-        Toast.makeText(getBaseContext(), "Login failed", Toast.LENGTH_LONG).show();
-
-        signUpButton.setEnabled(true);
+        signUpProgressDialog.dismiss();
+//        new android.os.Handler().postDelayed( //TODO: necessary?
+//                new Runnable() {
+//                    public void run() {
+//                        // On complete call either onSignUpSuccess or onSignUpFailed
+//                        // depending on success
+//                        onSignUpSuccess();
+//                        // onSignUpFailed();
+//
+//                    }
+//                }, 3000);
     }
 
     public boolean validate() {
-        boolean valid = true;
+        //TODO: call validation functions from utilities
+        return false;
+    }
 
-        String name = nameText.getText().toString();
-        String email = emailText.getText().toString();
-        String password = passwordText.getText().toString();
-
-        if (name.isEmpty() || name.length() < MIN_CHARACTERS_TO_NAME) {
-            nameText.setError("at least " + Integer.toString(MIN_CHARACTERS_TO_NAME) + " characters");
-            valid = false;
-        } else {
-            nameText.setError(null);
+    @Override
+    public void onClick(View view) {
+        switch (view.getId()) {
+            case R.id.btn_sign_up:
+                signUp();
+                break;
+            case R.id.link_login_sign_up_activity:
+                finish();
+                break;
         }
+    }
 
-        if (email.isEmpty() || !android.util.Patterns.EMAIL_ADDRESS.matcher(email).matches()) {
-            emailText.setError("enter a valid email address");
-            valid = false;
-        } else {
-            emailText.setError(null);
-        }
+    private void onSignUpFailed() {
+        Toast.makeText(getBaseContext(), getString(R.string.sign_up_failed), Toast.LENGTH_LONG).show();
+        btnSignUp.setEnabled(true);
+    }
 
-        if (password.isEmpty() || password.length() < MIN_PASSWORD_LENGTH) {
-            passwordText.setError("between 4 and 10 alphanumeric characters");
-            valid = false;
-        } else {
-            passwordText.setError(null);
-        }
+    private boolean addUserToDB() {
+        String name = etName.getText().toString();
+        String email = etEmail.getText().toString();
+        String password = etPassword.getText().toString();
+        String id = etId.getText().toString();
 
-        return valid;
+        // TODO: add the user to DB
+
+        return false;
     }
 }
