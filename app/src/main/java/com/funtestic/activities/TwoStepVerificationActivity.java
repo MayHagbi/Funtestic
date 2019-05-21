c2e1ab444dc247cb1d19b1ef3c74ea83b48a1e31
Yoel Vaizman <yoelv92@gmail.com>
2019-05-21 16:01:44 +0300

    create 2sv fage

--- app/src/main/java/com/funtestic/activities/TwoStepVerificationActivity.java
+++ app/src/main/java/com/funtestic/activities/TwoStepVerificationActivity.java
@@ -1,15 +1,45 @@
 package com.funtestic.activities;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 
 import com.funtestic.R;
+import com.funtestic.activities.report.ReportActivity;
 
-public class TwoStepVerificationActivity extends AppCompatActivity {
+public class TwoStepVerificationActivity extends AppCompatActivity implements View.OnClickListener {
+    private Button sendButton;
+    private EditText number;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_two_step_verification);
+
+        sendButton = (Button)findViewById(R.id.buttonSend2stepVerification);
+        number = (EditText) findViewById(R.id.editText2stepVerification);
+        sendButton.setOnClickListener(this);
+    }
+
+
+    @Override
+    public void onClick(View view){
+
+        switch(view.getId()) {
+
+            case R.id.buttonSend2stepVerification:
+                String num = number.getText().toString();
+                //get form db
+                //if(db == num)
+                //check input
+                finish();
+                //startActivity(new Intent(getApplicationContext() , MainActivity.class));
+                break;
+
+
+        }
     }
 }
