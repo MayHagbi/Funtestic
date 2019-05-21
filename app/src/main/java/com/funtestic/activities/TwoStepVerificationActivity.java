7b4ff5878782b24141fa2f3f46e729539b0a0e59
Yoel Vaizman <yoelv92@gmail.com>
2019-05-21 15:19:31 +0300

    create two step verification activity

---
+++ app/src/main/java/com/funtestic/activities/TwoStepVerificationActivity.java
@@ -0,0 +1,15 @@
+package com.funtestic.activities;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.funtestic.R;
+
+public class TwoStepVerificationActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_two_step_verification);
+    }
+}
