51da2eb3e2da584badca9489d29cbdc5f9f7fdfb
Yoel Vaizman <yoelv92@gmail.com>
2019-05-22 18:19:52 +0300

    http request fix

--- app/src/main/java/com/funtestic/models/Send_HTTP_Request.java
+++ app/src/main/java/com/funtestic/models/Send_HTTP_Request.java
@@ -4,31 +4,67 @@ import java.io.BufferedReader;
 import java.io.InputStreamReader;
 import java.net.HttpURLConnection;
 import java.net.URL;
+import java.security.Security;
+
+import javax.net.ssl.HttpsURLConnection;
+import javax.net.ssl.SSLContext;
+import javax.net.ssl.TrustManager;
+import javax.net.ssl.X509TrustManager;
 
 public class Send_HTTP_Request {
 
 
     public static String call_me() throws Exception {
-        String url = "https://www.google.com";
+        TrustManager[] trustAllCerts = new TrustManager[]{
+                new X509TrustManager() {
+
+                    public java.security.cert.X509Certificate[] getAcceptedIssuers()
+                    {
+                        return null;
+                    }
+                    public void checkClientTrusted(java.security.cert.X509Certificate[] certs, String authType)
+                    {
+                        //No need to implement.
+                    }
+                    public void checkServerTrusted(java.security.cert.X509Certificate[] certs, String authType)
+                    {
+                        //No need to implement.
+                    }
+                }
+        };
+        SSLContext sc = SSLContext.getInstance("SSL");
+        sc.init(null, trustAllCerts, new java.security.SecureRandom());
+        HttpsURLConnection.setDefaultSSLSocketFactory(sc.getSocketFactory());
+
+        String url = "https://www.mocky.io/v2/5185415ba171ea3a00704eed";
         URL obj = new URL(url);
         HttpURLConnection con = (HttpURLConnection) obj.openConnection();
-        // optional default is GET
-        con.setRequestMethod("GET");
+        con.setDoOutput(true);
+        con.setRequestProperty("Accept", "application/xml,text/xml,application/xhtml+xml");        // optional default is GET
+        //con.setRequestMethod("GET");
+
         //add request header
-        con.setRequestProperty("User-Agent", "Mozilla/5.0");
-        int responseCode = con.getResponseCode();
-        System.out.println("\nSending 'GET' request to URL : " + url);
-        System.out.println("Response Code : " + responseCode);
+        //con.setRequestProperty("User-Agent", "Mozilla/5.0");
+
+
+        //mack buffered reader--------------------------------
         BufferedReader in = new BufferedReader(
                 new InputStreamReader(con.getInputStream()));
+        //----------------------------------------------------
+
+        //Enters all information from the site to buffer
         String inputLine;
         StringBuffer response = new StringBuffer();
         while ((inputLine = in.readLine()) != null) {
             response.append(inputLine);
         }
+        //----------------------------------------------
+
         in.close();
 
         return response.toString();
 
     }
+
+
 }
