2fdbafc8b315c42c50d22f2a2139ff820bfeb6bd
Yoel Vaizman <yoelv92@gmail.com>
2019-06-02 18:38:25 +0300

    make TwoStepVerification request in DataBase

--- app/src/main/java/com/funtestic/activities/TwoStepVerificationActivity.java
+++ app/src/main/java/com/funtestic/activities/TwoStepVerificationActivity.java
@@ -1,6 +1,7 @@
 package com.funtestic.activities;
 
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
@@ -9,15 +10,30 @@ import android.widget.EditText;
 
 import com.funtestic.R;
 import com.funtestic.activities.report.ReportActivity;
+import com.funtestic.models.DataBase;
 
 public class TwoStepVerificationActivity extends AppCompatActivity implements View.OnClickListener {
     private Button sendButton;
     private EditText number;
+    //private SharedPreferences sp;
+    private String phone ;
+    private String password ;
+    private String token ;
+    private DataBase db ;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_two_step_verification);
+
+        phone=getIntent().getStringExtra("phone");
+        password=getIntent().getStringExtra("password");
+
+        token=db.TwoStepVerification(phone,password);
+        if(token == null){
+            //TODO ERROR
+        }
 
         sendButton = (Button)findViewById(R.id.buttonSend2stepVerification);
         number = (EditText) findViewById(R.id.editText2stepVerification);
