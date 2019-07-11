5b40a077dc0aa4999d8f78594fe1f4335072edb1
Yoni <jonbir3@gmail.com>
2019-07-11 21:37:39 +0300

    remove redundant "public" declarations from the interface

--- app/src/main/java/com/funtestic/interfaces/IDatabase.java
+++ app/src/main/java/com/funtestic/interfaces/IDatabase.java
@@ -2,38 +2,36 @@ package com.funtestic.interfaces;
 
 import com.funtestic.models.Child;
 import com.funtestic.models.Quiz;
-import com.funtestic.models.Report;
 import com.funtestic.models.User;
-
 import java.util.ArrayList;
 
 public interface IDatabase {
 
     //Get User by email
-    public User getUserByPhone(String phone ,final String token)throws Exception;
+    User getUserByPhone(String phone ,final String token)throws Exception;
 
     //Get child by ID
-    public Child getChildById(String id ,final String token);
+    Child getChildById(String id ,final String token);
 
     //Get all User children
-    public ArrayList<Child> getUserChildren(String phone ,final String token);
+    ArrayList<Child> getUserChildren(String phone ,final String token);
 
     //Add new user to the database
-    public boolean addUserToDb(User user);
+    boolean addUserToDb(User user);
 
     //Add new child to the database
-    public boolean addChildToDb(Child child,final String token);
+    boolean addChildToDb(Child child,final String token);
 
     //Add quiz to the database
-    public boolean addQuizToDb(Quiz quiz ,final String token);
+    boolean addQuizToDb(Quiz quiz ,final String token);
 
     //Send Report To Email
-    public boolean sendReportToEmail(String childId ,final String token);
+    boolean sendReportToEmail(String childId ,final String token);
 
     //Check if user is valid for login purpose
-    public boolean login(String phone,String password);
+    boolean login(String phone,String password);
 
     //Two factor authentication
-    public String TwoStepVerification(String phone,String password , String TwoFA_pass );
+    String TwoStepVerification(String phone,String password , String TwoFA_pass );
 
 }
